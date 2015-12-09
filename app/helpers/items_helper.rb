ca9d73a99a6e4c7fbd714211c4c1171971b22f14
Jason Teitelman <super.soviet.jesus@gmail.com>
2015-12-09 19:37:39 +0000

    Display time till deletion

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,14 @@
 module ItemsHelper
+  
+  def inactive?(item)
+    !(item.active?)
+  end
+  
+  def time_to_archive(activated_at)
+    distance_of_time_in_words(activated_at, activated_at + 7)
+  end
+  
+  def time_to_delete(deactivated_at)
+    distance_of_time_in_words(deactivated_at, deactivated_at + 30)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,16 @@ user.save!
 10.times {Item.create!(user: user, name: Faker::Lorem.sentence )}
 5.times {Item.create!(user: user, name: Faker::Lorem.sentence, state: :completed)}
 3.times {Item.create!(user: user, name: Faker::Lorem.sentence, state: :archived)}
+Item.active.each do |i|
+  i.activate
+  i.save!
+end
+Item.completed.each do |i|
+  i.deactivate(:completed)
+  i.save!
+end
+Item.archived.each do |i|
+  i.deactivate(:archived)
+  i.save!
+end
 puts "seeded user jason@example.com"
